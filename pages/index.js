16a02c45b64b1ab0d8379a2441c034e80542020a
Thinh Le <thinhle@gocodee.com>
2019-03-08 10:32:22 +0700

    push

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,18 @@ import Link from "next/link";
 import Header from "../components/header";
 
 function Index() {
+  componentDidMount () {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => {
+          console.log('service worker registration successful')
+        })
+        .catch(err => {
+          console.warn('service worker registration failed', err.message)
+        })
+    }
+  }
   return (
     <main>
       <Header />
