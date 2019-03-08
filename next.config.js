faa2d70e5f009bc564b23451da4fcf49f249ae5b
Thinh Le <thinhle@gocodee.com>
2019-03-08 10:30:00 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
+// module.exports = {
+//   target: 'serverless'
+// }
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+
 module.exports = {
-  target: 'serverless'
+  webpack: config => {
+    config.plugins.push(
+      new SWPrecacheWebpackPlugin({
+        verbose: true,
+        navigateFallback: '/index',
+        staticFileGlobsIgnorePatterns: [/\.next\//],
+        runtimeCaching: [
+          {
+            handler: 'networkFirst',
+            urlPattern: /^https?.*/
+          }
+        ]
+      })
+    )
+
+    return config
+  }
 }
